576446afdbe8aaa5e2026e29d203e8598582fa78
Балакина Екатерина Петровна <114657505+BalakinaEP@users.noreply.github.com>
2025-03-03 12:51:58 +0300

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,4 +1,5 @@
 #incude <cmath>
+#incude <string>
 #incude "Complex.h"
 
 Complex::Complex(const double re = 0, const double im = 0):
@@ -37,6 +38,13 @@ double Complex::operator*(const Complex& other)
   return Complex(newRe, newIm);
 }
 
+Complex Complex::operator*(const double other)
+{
+    double newRe = re * other;
+    double newIm = im * other;
+    return Complex(newRe, newIm);
+}
+
 double Complex::operator/(const Complex& other)
 {
   double newRe = (re * other.re + im * other.im) / (other.re * other.re + other.im * other.im);
@@ -47,4 +55,50 @@ double Complex::operator/(const Complex& other)
 bool Complex::operator==(const Complex& other)
 {
   return re == other.re && im == other.im
+}
+
+bool Complex::operator!=(const Complex& other)
+{
+  return !(*this == other);
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& z)
+{
+  os << z.re;
+  if (z.im < 0)
+  {
+    os << z.im << "i" << std::endl;
+  }
+  else
+  {
+    os << "+" << z.im << "i" << std::endl;
+  }
+}
+
+std::istream& operator>>(std::istream& is, const Complex& z)
+{
+  cout << " Введите действительную часть числа: ");
+  is >> z.re;
+  cout << " Введите мнимую часть числа: ");
+  is >> z.im;
+  return is;
+}
+
+Complex operator*(const double other, const Complex& z)
+{
+  return z * other; 
+}
+
+std::string toString() const
+{
+  std::string str = to_string(re);
+  if (im < 0)
+  {
+    str += to_string(im) + "i\n";
+  }
+  else
+  {
+    str += "+" + to_string(im) + "i\n";
+  }
+  return str;
 }
